94fe80e9e3e9ff26456b95a21ed0466139f3a9cc
Laszlo Somogyi <slsomogyilaszlo@gmail.com>
2021-04-22 11:03:18 +0200

    JSF managed bean initialized

---
+++ src/java/mgbeans/Lister.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mgbeans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author Somogyi László <proceed step by step>
+ */
+@ManagedBean
+@SessionScoped
+public class Lister {
+
+    /**
+     * Creates a new instance of Lister
+     */
+    public Lister() {
+    }
+    
+}
